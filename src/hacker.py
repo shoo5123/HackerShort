7f4002f998924cd7c9c7ac6d1c57992e4701abd9
shuta <sdsds.com@hotmail.co.jp>
2022-11-22 01:22:01 +0900

    sleep 修正

--- src/hacker.py
+++ src/hacker.py
@@ -52,7 +52,9 @@ def main():
                 logger.info("鯨がうごきました")
                 logger.info("https://etherscan.io/address/0x59abf3837fa962d6853b4cc0a19513aa031fd32b")
                 create_short_entry(symbol, side, type, quantity)
-        logger.info("まだ鯨は動いてないよ")
+                time.sleep(300)
+                break
+            logger.info("まだ鯨は動いてないよ")
         # 1min おきに実行
         time.sleep(60)
 
